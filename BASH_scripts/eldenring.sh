1bcfa1bf0aec524c687cc93a243e5ee214a3fab2
ridelsam <ridel.asam@gmail.com>
2022-09-10 14:57:27 -0700

    conditional statement with  elden game

--- BASH_scripts/eldenring.sh
+++ BASH_scripts/eldenring.sh
@@ -1,9 +1,35 @@
-
-echo
-"You Died"
 #!/bin/bash
 
 echo "You Died"
+
+echo "Welcome tarnished. Please choose your starting class
+1 - Samurai
+2 - Prisoner
+3 - Prophet
+
+read class
+
+case $class in 
+
+	1)
+		type="Samurai"
+		hp=10
+		attack=20
+		;;
+	2)
+		type="Prisoner"
+		hp=20
+		attack=4;
+		;;
+	3)
+		type="Prophet"
+		hp=30
+		attack=4
+		;;
+esac
+
+echo "you have chosen the $type class. Your HP is $hp and your attack is $attack."
+
 
 
 #first beast battle
